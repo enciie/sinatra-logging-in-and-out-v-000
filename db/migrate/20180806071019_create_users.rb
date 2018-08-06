d411225df85513e08cc7c85f5ed018552bc61339
Nancy Jo <enciie@gmail.com>
2018-08-06 07:11:29 +0000

    automatically backed up by learn

--- db/migrate/20180806071019_create_users.rb
+++ db/migrate/20180806071019_create_users.rb
@@ -1,4 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
+    create_table :users do |t|
+      t.string :username
+      t.srting :password
+      t.decimal :balance
+    end
   end
 end
