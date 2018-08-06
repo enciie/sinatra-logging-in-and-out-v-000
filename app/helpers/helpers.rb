9b4d66657d8c2219a2f9161bad5f1ecc043fe09e
Nancy Jo <enciie@gmail.com>
2018-08-06 07:46:21 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -3,11 +3,11 @@ require 'sinatra/base'
 class Helpers
 
   def self.current_user(session)
-    @user = User.find_by_id(params[:user_id])
+    @user = User.find_by_id(params[:id])
   end
-
+  
   def self.is_logged_in?(session)
-    !!session[:user_id]
+    !!session[:id]
   end
   
 end
