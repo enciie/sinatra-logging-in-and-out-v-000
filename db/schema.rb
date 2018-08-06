1544fb54376db3174fb8fed1ad3807b6df734871
Nancy Jo <enciie@gmail.com>
2018-08-06 07:26:10 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20180806071019) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
+
 end
